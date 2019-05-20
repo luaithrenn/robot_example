31f84b556b6596e45a96e5a6ce7116b431aa6b3a
luaithrenn <luaidemp@ie.ibm.com>
2019-05-20 16:21:12 +0100

    Create MultiplyByFactor.py

---
+++ multiplybyfactor/MultiplyByFactor.py
@@ -0,0 +1,42 @@
+import datetime as dt
+import json
+import pandas as pd
+import numpy as np
+from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean, func
+from iotfunctions.base import BaseTransformer
+from iotfunctions.metadata import EntityType
+from iotfunctions.db import Database
+from iotfunctions import ui
+
+class MultiplyByFactor(BaseTransformer):
+    
+    def __init__(self, input_items, factor, output_items):
+                
+        self.input_items = input_items
+        self.output_items = output_items
+        self.factor = float(factor)
+        super().__init__()
+
+    def execute(self, df):
+        df = df.copy()
+        for i,input_item in enumerate(self.input_items):
+            df[self.output_items[i]] = df[input_item] * self.factor
+        return df
+    
+    @classmethod
+    def build_ui(cls):
+        #define arguments that behave as function inputs
+        inputs = []
+        inputs.append(ui.UIMultiItem(
+                name = 'input_items',
+                datatype=float,
+                description = "Data items adjust",
+                output_item = 'output_items',
+                is_output_datatype_derived = True)
+                      )        
+        inputs.append(ui.UISingle(
+                name = 'factor',
+                datatype=float)
+                      )
+        outputs = []
+        return (inputs,outputs)        
